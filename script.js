3c31846147ae224de6063e01dd594ec4b999524d
darrionn33 <darrionn33@gmail.com>
2023-10-14 19:38:39 +0530

    made AC & C button functional

--- script.js
+++ script.js
@@ -4,14 +4,36 @@ const buttons = document.querySelectorAll("button");
 const calculatorDisplayArray = [0];
 calculatorScreen.textContent = calculatorDisplayArray;
 
+let isInitialising = true;
 const updateDisplayArray = (item) => {
-  calculatorDisplayArray.push(item);
+  if (item) {
+    if (isInitialising) {
+      calculatorDisplayArray[0] = item;
+      isInitialising = false;
+    } else {
+      calculatorDisplayArray.push(item);
+    }
+  }
   calculatorScreen.textContent = calculatorDisplayArray.join("");
 };
 
 buttons.forEach((button) =>
   button.addEventListener("click", (e) => {
     let input = e.target.value;
-    updateDisplayArray(input);
+    switch (input) {
+      case "ac":
+        calculatorDisplayArray.length = 0;
+        calculatorDisplayArray[0] = 0;
+        isInitialising = true;
+        updateDisplayArray();
+        break;
+      case "c":
+        calculatorDisplayArray.pop();
+        updateDisplayArray();
+        break;
+      default:
+        updateDisplayArray(input);
+        break;
+    }
   })
 );
